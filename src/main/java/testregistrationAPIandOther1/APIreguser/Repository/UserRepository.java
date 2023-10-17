6fb8b4c51c173ac5e1f6b9ff3473128ad7f58d08
Volosha <71184474+Vo1osha@users.noreply.github.com>
2023-10-17 11:22:43 +0300

    Update UserRepository.java

--- src/main/java/testregistrationAPIandOther1/APIreguser/Repository/UserRepository.java
+++ src/main/java/testregistrationAPIandOther1/APIreguser/Repository/UserRepository.java
@@ -2,6 +2,6 @@ package testregistrationAPIandOther1.APIreguser.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import testregistrationAPIandOther1.APIreguser.Entity.UserEntity;
-// DAO?
+// repository pattern?
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 }
